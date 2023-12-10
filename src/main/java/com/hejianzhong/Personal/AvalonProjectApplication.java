3a92f39dfadcbfa04a0b8862437464d0d6616a92
Jianzhong He <hejianzhong@berkeley.edu>
2023-12-10 15:51:38 -0800

    initial commit

---
+++ src/main/java/com/hejianzhong/Personal/AvalonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hejianzhong.Personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvalonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvalonProjectApplication.class, args);
+	}
+
+}
